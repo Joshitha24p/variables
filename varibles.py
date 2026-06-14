aef1a5df05bdb85d0a8ebbe2201d49a83efb87b1
Joshitha24p <joshitha.joshi2012@gmail.com>
2026-06-14 10:35:12 -0400

    Add files via upload

---
+++ varibles.py
@@ -0,0 +1,20 @@
+x=5
+y="Joshitha"
+print (x)
+print (y)
+codinggal="coding"
+print (codinggal)
+
+print ("Registration form")
+name=input ("Enter your name")
+age=input ("enter your age")
+grade=input ("enter your grade")
+school=input ("Name of your school")
+city=input ("enter your city")
+hobbies=input ("Enter your hobbies")
+print (name)
+print (age)
+print (grade)
+print (school)
+print (city)
+print (hobbies)
